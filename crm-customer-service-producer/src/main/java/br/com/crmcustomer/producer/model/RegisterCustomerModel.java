c86294a2c14f8ea38cdd552f72561c832108dd67
tqi_maraujo <tqi_maraujo@uolinc.com>
2022-06-29 09:05:58 -0300

    feature/CRM-CONSUMER switch RegisterCustomerModel to record.

--- crm-customer-service-producer/src/main/java/br/com/crmcustomer/producer/model/RegisterCustomerModel.java
+++ crm-customer-service-producer/src/main/java/br/com/crmcustomer/producer/model/RegisterCustomerModel.java
@@ -1,65 +1,17 @@
 package br.com.crmcustomer.producer.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class RegisterCustomerModel {
-    private String name;
-    private String motherName;
-    private String documentContent;
-    private String contactContent;
-    private String addressStreet;
-    private String addressNumber;
+public record RegisterCustomerModel (
+         String name,
+         String motherName,
+         String documentContent,
+         String contactContent,
+         String addressStreet,
+         String addressNumber){
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMotherName() {
-        return motherName;
-    }
-
-    public void setMotherName(String motherName) {
-        this.motherName = motherName;
-    }
-
-    public String getDocumentContent() {
-        return documentContent;
-    }
-
-    public void setDocumentContent(String documentContent) {
-        this.documentContent = documentContent;
-    }
-
-    public String getContactContent() {
-        return contactContent;
-    }
-
-    public void setContactContent(String contactContent) {
-        this.contactContent = contactContent;
-    }
-
-    public String getAddressStreet() {
-        return addressStreet;
-    }
-
-    public void setAddressStreet(String addressStreet) {
-        this.addressStreet = addressStreet;
-    }
-
-    public String getAddressNumber() {
-        return addressNumber;
-    }
-
-    public void setAddressNumber(String addressNumber) {
-        this.addressNumber = addressNumber;
-    }
 
     @Override
     public String toString() {
