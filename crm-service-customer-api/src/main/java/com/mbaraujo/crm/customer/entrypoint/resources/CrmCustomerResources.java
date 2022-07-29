e3d887551020a19ca70f487bbed09662895b4625
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-07-29 00:07:55 -0300

    add logs, fix server port

--- crm-service-customer-api/src/main/java/com/mbaraujo/crm/customer/entrypoint/resources/CrmCustomerResources.java
+++ crm-service-customer-api/src/main/java/com/mbaraujo/crm/customer/entrypoint/resources/CrmCustomerResources.java
@@ -1,16 +1,19 @@
 package com.mbaraujo.crm.customer.entrypoint.resources;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 @RequestMapping("/")
 public class CrmCustomerResources {
 
     @GetMapping
     public ResponseEntity hello(){
+        log.info("Works fine. method [hello]");
         return ResponseEntity.ok("Hello!");
     }
 
