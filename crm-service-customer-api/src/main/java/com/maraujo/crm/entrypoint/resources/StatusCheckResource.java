81dd95c57fc89fe0a8c9b2144e85b0794d4fbb41
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-08-01 19:13:34 -0300

    add controller to statuscheck

--- crm-service-customer-api/src/main/java/com/maraujo/crm/entrypoint/resources/StatusCheckResource.java
+++ crm-service-customer-api/src/main/java/com/maraujo/crm/entrypoint/resources/StatusCheckResource.java
@@ -2,7 +2,9 @@ package com.maraujo.crm.entrypoint.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/")
 public class StatusCheckResource {
 
